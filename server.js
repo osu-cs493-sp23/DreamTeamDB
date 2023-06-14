ea381250b7dc761da46b73d65c3eff122be07983
Artem <91341345+artkolpakov@users.noreply.github.com>
2023-06-14 02:30:13 -0700

    [PR] Implemented rate limiting using Redis Client (#9)

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ import mongoose from "mongoose";
 import dotenev from "dotenv";
 import morgan from "morgan";
 import cors from "cors";
+import RedisStore from "rate-limit-redis";
+import rateLimit from "express-rate-limit";
+import redis from "redis";
+import { rateLimitM } from "./lib/auth.js";
 
 import api from "./api/index.js";
 
@@ -27,12 +31,51 @@ const mongoDatabase = process.env.MONGO_DBNAME || "tarpaulin";
 const mongoUser = process.env.MONGO_USER || "admin";
 const mongoPassword = process.env.MONGO_PASS || "admin";
 const mongoConnectionString = `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}/${mongoDatabase}`;
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+})
+
+// Create a rate limiter middleware
+export const limiterUnauthenticated = rateLimit({
+  windowMs: 60 * 1000,  // 1 minute
+  max: 10,              // for requests made without a valid authentication token, the API permits 10 requests per minute
+  standardHeaders: true,
+  message: "Unauthenticated requests are limited to 10 per minute.",
+  store: new RedisStore({
+    sendCommand: (...args) => {
+      return client.sendCommand(...args);
+    }
+  })
+});
+
+export const limiterAuthenticated = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 30,              // for requests made with a valid authentication token, the API permits 30 requests per minute
+  standardHeaders: true,
+  message: "Authenticated requests are limited to 30 per minute.",
+  store: new RedisStore({
+    sendCommand: (...args) => {
+      return client.sendCommand(...args);
+    }
+  })
+});
+
+async function connectToRedis() {
+  try {
+    await redisClient.connect();
+    // console.log("Redis client connected");
+  } catch (err) {
+    console.log("Redis error: ", err);
+  }
+}
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(cors())
+app.use(rateLimitM)
 
 // API
 app.use("/api", api);
@@ -66,6 +109,14 @@ mongoose
     app.listen(port, () => {
       console.log(`[⚡️ SERVER] Server is running on port ${port}`);
     });
+  });
+
+connectToRedis()
+  .then(() => {
+    console.log(`[⚡️ SERVER] Redis is running on port ${process.env.REDIS_PORT}`);
+  })
+  .catch((err) => {
+    console.log("Error connecting to Redis: ", err);
   });
 
 /*
