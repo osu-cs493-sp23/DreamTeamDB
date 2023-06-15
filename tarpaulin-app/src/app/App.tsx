bda2e619d355579ac89c7d043b49f68f073e4808
alaric <alaric.spam@protonmail.com>
2023-06-15 03:35:24 -0700

    [PR] Courses CRUD Implementation (#10)
    * Sending the ID and Role to the Client
    
    * Added User Context Provider
    
    * Added Key to Testimonials
    
    * Added useData Hook and mock data
    
    * Updated Login to store creds in LS
    
    * Added assignments, single course, and dashboard
    
    * Frontend Changes
    
    * Courses
    
    * [Merge]  Create submission -> Courses (#14)
    
    * [PR] Courses CRUD Integration  (#7)
    * Sending the ID and Role to the Client
    * Added User Context Provider
    * Added Key to Testimonials
    * Added useData Hook and mock data
    * Updated Login to store creds in LS
    * Added assignments, single course, and dashboard
    * Frontend Changes
    * Courses
    
    * [PR] (1) mock data (2) localStorage client creds (3) dashboard etc. component  (#6)
    
    * Sending the ID and Role to the Client
    
    * Added User Context Provider
    
    * Added Key to Testimonials
    
    * Added useData Hook and mock data
    
    * Updated Login to store creds in LS
    
    * Added assignments, single course, and dashboard
    
    * Frontend Changes
    
    * [PR] Frontend Work (1) connected to b.e (2) crud courses for admins/instructors (#8)
    
    * Sending the ID and Role to the Client
    
    * Added User Context Provider
    
    * Added Key to Testimonials
    
    * Added useData Hook and mock data
    
    * Update Login to store creds in LS
    
    * Added assignments, single course, and dashboard
    * Frontend Changes
    * [PR] Implemented rate limiting using Redis Client (#9)
    * Completed /POST /assignments/{id}/submissions
    * Frontend
    * Backend
    ---------
    
    Co-authored-by: Artem <91341345+artkolpakov@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Thomas <nyumat18@gmail.com>
    Co-authored-by: Tom Nyuma <46255836+Nyumat@users.noreply.github.com>
    Co-authored-by: Artem <91341345+artkolpakov@users.noreply.github.com>

--- tarpaulin-app/src/app/App.tsx
+++ tarpaulin-app/src/app/App.tsx
@@ -5,6 +5,8 @@ import Landing from '../pages/main/Landing'
 import { Flex, IconButton, useColorMode } from '@chakra-ui/react'
 import { BsMoonStarsFill, BsFillSunFill } from 'react-icons/bs'
 import Dashboard, { Content } from '../pages/main/Dashboard'
+import { useContext } from 'react'
+import { UserContext } from '../context/UserContext'
 import { useDispatch } from 'react-redux'
 import React from 'react'
 import { fetchCourses } from '../redux/CourseSlice'
@@ -12,6 +14,8 @@ import { fetchCourses } from '../redux/CourseSlice'
 function App() {
 
   const dispatch = useDispatch()
+  // const { user } = useContext(UserContext);
+  // const role = user?.role;
 
   React.useEffect(() => {
     dispatch(fetchCourses() as any)
